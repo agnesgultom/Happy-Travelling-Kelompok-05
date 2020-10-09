e68e812385e091203458b3d053b38ddf4b0dc0f9
agnesgultom <agnesgultom9@gmail.com>
2020-10-09 10:42:35 +0700

    first

---
+++ src/main/java/del/ac/id/demo/HappyTravelingKelompok5Application.java
@@ -0,0 +1,13 @@
+package del.ac.id.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyTravelingKelompok5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyTravelingKelompok5Application.class, args);
+	}
+
+}
